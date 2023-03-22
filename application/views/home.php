7f2441de70d8b2f0872f6daf911bb13db41ff18a
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-23 02:13:09 +0800

    Homepage work

--- application/views/home.php
+++ application/views/home.php
@@ -278,53 +278,10 @@ $base_url = base_url();
 						</div>
 					</div>
 				</section>
-
-
-				<article class="work-tile fw media-within bottom" data-out="fade">
-					<a href="/work/goodles/">
-						<video class="bg auto miq preload-critical mr" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/goodles-home-hero-case-study-alt_Converted.mp4"></video>
-						<img class="bg miq preload-critical dr" data-preload-mobile="<?php echo base_url(); ?>/assets/img/demo/171df1ed-720b-412a-bd87-86ea4dde808e_goodles-mobile-feature-hero.jpg?q=65&w=550&auto=format" alt="">
-
-						<div class="copy cml-60">
-							<h1>GOODLES</h1>
-							<p>MAC'S BACK BABY</p>
-						</div>
-					</a>
-				</article>
-				<article class="work-tile fw media-within center" data-out="fade">
-					<a href="/work/kizik/" data-entrance="cover-copy" data-offset=".85" data-offset-mobile=".85">
-						<video class="bg auto miq preload" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/kizik-case-study_hero-edit-desktop-compressed.mp4" data-preload-mobile="<?php echo base_url(); ?>/assets/video/demo/kizik-case-study_hero-edit-desktop-compressed.mp4"></video>
-
-						<div class="copy cml-60">
-							<h1>Kizik</h1>
-							<p>Stepping it up with Kizik.</p>
-						</div>
-					</a>
-				</article>
-				<article class="work-tile fw media-within bottom" data-out="fade">
-					<a href="/work/nylon/" data-entrance="cover-copy" data-offset=".85" data-offset-mobile=".85">
-						<video class="bg auto miq preload mr" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/NYLON-vid-2.mp4"></video>
-
-						<div class="copy cml-60 color-override" style="--primary-override: #ffffff">
-							<h1>Nylon</h1>
-							<p>Redesigning an Iconic Magazine for the Internet Age.</p>
-						</div>
-					</a>
-				</article>
-				<article class="work-tile fw media-within bottom" data-out="fade">
-					<a href="/work/windmill/" data-entrance="cover-copy" data-offset=".85" data-offset-mobile=".85">
-						<video class="bg auto miq preload" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/Windmill-spin.mp4" data-preload-mobile="<?php echo base_url(); ?>/assets/video/demo/Windmill-spin.mp4"></video>
-
-						<div class="copy cml-60 color-override" style="--primary-override: #000000">
-							<h1>Windmill</h1>
-							<p>Launching the breeziest AC brand around.</p>
-						</div>
-					</a>
-				</article>
 				<div class="c-60 all-work-button" data-out="fade">
 					<div data-entrance="wipe-bg" data-offset=".98" data-offset-mobile=".8">
 						<div class="wipe-bg"></div>
-						<a href="/work/" class="banner-button">See all work</a>
+						<a href="/work/" class="banner-button">See all Portfolio</a>
 					</div>
 				</div>
 			</div>
@@ -332,8 +289,11 @@ $base_url = base_url();
 
 		<section class="about-callout c-120">
 			<div data-out="fade" data-entrance="basic-fade" data-offset=".86" data-offset-mobile=".85">
-				<h2>Zero is a digitally-native branding agency with offices in New York and Milwaukee. We build next generation brands and digital experiences.</h2>
-				<a class="read-more underline" href="/about-us/">Read more<span class="visuallyhidden">about us</span></a>
+				<div class="lets-chat-div">
+					<p>LET'S CHAT</p>
+					<h2>Have a Project, Let us Help.</h2>
+					<a class="read-more underline" href="/about-us/">GET STARTED</a>
+				</div>
 			</div>
 
 
@@ -341,10 +301,10 @@ $base_url = base_url();
 			<div class="quick-contact" data-entrance="wipe-bg" data-offset=".86" data-offset-mobile=".8" data-out="fade">
 				<div class="wipe-bg"></div>
 
-				<a href="https://www.instagram.com/zero.studios/" target="_blank" target="_blank" rel="noopener" class="handle banner-button">@Zero.Studios
+				<a href="https://www.instagram.com/zero.studios/" target="_blank" target="_blank" rel="noopener" class="handle banner-button">© 2022 Clarity
 				</a>
 
-				<a href="mailto:some1@zero.nyc" class="email banner-button">SOME1@ZERO.NYC
+				<a href="mailto:info@clarity.lt" class="email banner-button">info@clarity.lt
 				</a>
 			</div>
 
@@ -352,23 +312,36 @@ $base_url = base_url();
 			<div class="office-snapshots media-within" data-entrance="stagger-fade" data-offset=".86" data-offset-mobile=".85">
 				<div class="office parallax-hover s-el" data-out="fade">
 					<div class="ar-wrapper" style="--dar: 117.5%; --mar: 117.5%;">
-						<img class="bg preload" data-preload-desktop="<?php echo base_url(); ?>/assets/img/demo/2c8777a8-c75f-4390-8945-3bba48242421_zero-nyc-canal-street.png?auto=compress,format&rect=0,0,800,940&w=800&h=940" alt="">
+						<img class="bg preload" data-preload-desktop="<?php echo base_url(); ?>/assets/img/Clarity_Logo_Site.jpg" alt="">
 					</div>
 					<address class="address move-el">
-						<p>NYC<br>
-							Brooklyn</p>
+						<h3><a href="javascript:void(0);" class="modal-trigger" data-modal="mark-goldwell" aria-label="Read Bio">40+
+							</a></h3>
+						<p>Companies Built</p>
 					</address>
 				</div>
 				<div class="office parallax-hover s-el" data-out="fade">
 					<div class="ar-wrapper" style="--dar: 117.625%; --mar: 117.5%;">
-						<img class="bg preload" data-preload-desktop="<?php echo base_url(); ?>/assets/img/demo/c134093a-e33f-4122-a694-89395cac3fdf_MKE_office.jpg?auto=compress,format&rect=0,0,740,870&w=800&h=941" alt="">
+						<img class="bg preload" data-preload-desktop="<?php echo base_url(); ?>/assets/img/Clarity_Logo_Site.jpg" alt="">
 					</div>
 					<address class="address move-el">
-						<p>MKE<br>
-							Third Ward</p>
+						<h3><a href="javascript:void(0);" class="modal-trigger" data-modal="joseph-f.-delaney-iii" aria-label="Read Bio">6+
+							</a></h3>
+						<p>Portfolio Exits</p>
+					</address>
+				</div>
+				<div class="office parallax-hover s-el" data-out="fade">
+					<div class="ar-wrapper" style="--dar: 117.625%; --mar: 117.5%;">
+						<img class="bg preload" data-preload-desktop="<?php echo base_url(); ?>/assets/img/Clarity_Logo_Site.jpg" alt="">
+					</div>
+					<address class="address move-el">
+						<h3><a href="javascript:void(0);" class="modal-trigger" data-modal="chelsea-goldwell" aria-label="Read Bio">130+
+							</a></h3>
+						<p>In House Developers</p>
 					</address>
 				</div>
 			</div>
 		</section>
+
 	</div>
 </main>
---
+++ application/views/temp.php
@@ -0,0 +1,41 @@
+<article class="work-tile fw media-within bottom" data-out="fade">
+   <a href="/work/goodles/">
+      <video class="bg auto miq preload-critical mr" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/goodles-home-hero-case-study-alt_Converted.mp4"></video>
+      <img class="bg miq preload-critical dr" data-preload-mobile="<?php echo base_url(); ?>/assets/img/demo/171df1ed-720b-412a-bd87-86ea4dde808e_goodles-mobile-feature-hero.jpg?q=65&w=550&auto=format" alt="">
+
+      <div class="copy cml-60">
+         <h1>GOODLES</h1>
+         <p>MAC'S BACK BABY</p>
+      </div>
+   </a>
+</article>
+<article class="work-tile fw media-within center" data-out="fade">
+   <a href="/work/kizik/" data-entrance="cover-copy" data-offset=".85" data-offset-mobile=".85">
+      <video class="bg auto miq preload" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/kizik-case-study_hero-edit-desktop-compressed.mp4" data-preload-mobile="<?php echo base_url(); ?>/assets/video/demo/kizik-case-study_hero-edit-desktop-compressed.mp4"></video>
+
+      <div class="copy cml-60">
+         <h1>Kizik</h1>
+         <p>Stepping it up with Kizik.</p>
+      </div>
+   </a>
+</article>
+<article class="work-tile fw media-within bottom" data-out="fade">
+   <a href="/work/nylon/" data-entrance="cover-copy" data-offset=".85" data-offset-mobile=".85">
+      <video class="bg auto miq preload mr" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/NYLON-vid-2.mp4"></video>
+
+      <div class="copy cml-60 color-override" style="--primary-override: #ffffff">
+         <h1>Nylon</h1>
+         <p>Redesigning an Iconic Magazine for the Internet Age.</p>
+      </div>
+   </a>
+</article>
+<article class="work-tile fw media-within bottom" data-out="fade">
+   <a href="/work/windmill/" data-entrance="cover-copy" data-offset=".85" data-offset-mobile=".85">
+      <video class="bg auto miq preload" preload muted loop playsinline data-preload-desktop="<?php echo base_url(); ?>/assets/video/demo/Windmill-spin.mp4" data-preload-mobile="<?php echo base_url(); ?>/assets/video/demo/Windmill-spin.mp4"></video>
+
+      <div class="copy cml-60 color-override" style="--primary-override: #000000">
+         <h1>Windmill</h1>
+         <p>Launching the breeziest AC brand around.</p>
+      </div>
+   </a>
+</article>
